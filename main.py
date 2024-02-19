4ceb49f8b31d36b8eb169af98256d28fdc998ab5
Migueloc32 <159493278+Migueloc32@users.noreply.github.com>
2024-02-19 17:49:13 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+class Asiento:
+    def __init__(self):
+        self.color = ""
+        self.precio = 0
+        self.registro = 0
+
+    def cambiar_color(self, color):
+        colores_validos = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if color in colores_validos:
+            self.color = color
+
+
+class Motor:
+    def __init__(self):
+        self.numero_cilindros = 0
+        self.tipo = ""
+        self.registro = 0
+
+    def cambiar_registro(self, registro):
+        self.registro = registro
+
+    def asignar_tipo(self, tipo):
+        tipos_validos = ["electrico", "gasolina"]
+        if tipo in tipos_validos:
+            self.tipo = tipo
+
+
+class Auto:
+    cantidad_creados = 0
+
+    def __init__(self):
+        self.modelo = ""
+        self.precio = 0
+        self.asientos = []
+        self.marca = ""
+        self.motor = Motor()
+        self.registro = 0
+        Auto.cantidad_creados += 1
+
+    def cantidad_asientos(self):
+        total_asientos = 0
+        for asiento in self.asientos:
+            if asiento is not None:
+                total_asientos += 1
+        return total_asientos
+
+    def verificar_integridad(self):
+        if self.motor.registro == self.registro:
+            for asiento in self.asientos:
+                if asiento is not None:
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        return "Las piezas no son originales"
